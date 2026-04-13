c324ad7368f58bdb327260513fb3da2fdb8fd290
Sven Kuncikowski <sven.kuncikowski@ext.ec.europa.eu>
2026-04-13 11:41:24 +0200

    put all the scripts in the right place

---
+++ scripts/activate-all.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ln ~/dotfiles/scripts/tcg-prod.sh ~/.local/bin/tcg-prod.sh
+ln ~/dotfiles/scripts/tcg-serve.sh ~/.local/bin/tcg-serve.sh
+ln ~/dotfiles/scripts/monitor-menu.sh ~/.local/bin/monitor-menu.sh
+ln ~/dotfiles/scripts/monitor-profile.sh ~/.local/bin/monitor-profile.sh
